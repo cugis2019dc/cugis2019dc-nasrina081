bafc8572747742ba3891ee087cadc4669296de70
nasrina081 <53440444+nasrina081@users.noreply.github.com>
2019-07-31 10:06:57 -0400

    Add files via upload

---
+++ day 2 coding.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:48:52 2019
+
+@author:nasrin STEM
+"""
+def cadburry(a):
+    cadburry = a+10
+    
+    print(cadburry)
+    
+cadburry(5)
+
+
+def flowers(f):
+    flowers = f+5
+    
+    print(flowers)
+    
+flowers(3)
+
+def kittens(k):
+    kittens = k+4
+    
+    print(kittens)
+    
+kittens(45)
+
+def cadburry(c):
+    cadburry = c+5
+    
+    print (cadburry)
+
+cadburry(15)
+
+
+cadburryType1= "milk chocolate"
+cadburryType2= "dark chocolate"
+cadburryType3= "white chocolate"
+
+def cadburrybox(w,d,m):
+    print("there is", m, ",", d, w, "in the box")
+
+cadburrybox("white chocolate", "dark chocolate", "milk chocolate")
+   
+print("hey nasrin")
+
+a="nasrin"
+print("hello",a)
+
+name = input("please enter your name")
+
+print("your name is", name)
+
+print('enter your age:')
+X = input()
+print('your age is' + X)
+
+#library
+import math
+
+dir(math)
+
+math.pow()
+math.factorial(7)
+
+def cubicroot(a):
+    print("the cubic root of",a,"is",math.pow(a,1/3))
+
+cubicroot(8)
+
+def cubic(x):
+    cubic=math.pow(x,.33)
+    print("the cubic root of",X, "is",cubic,)
+    
+cubic(8)
+    
+
+cubicroot = input("please enter a value for your variable")
+
+cubicroot = input ('"Please enter a value for your variable")
+ y = (number)**2
+ z = (number)**3
+ print (y+z)
+ 
+ import math
+ a= int(input("enter a value"))
+ 
+ def cadbury(a):
+     cadbury=math.pow(a,1/3)
+     print("the cubicroot of", a, "is",cadbury)
+     
+cadbury(a)
+     
+
+
+
+   
